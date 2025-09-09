884f0f6ef85687a10241841e5cd72cf1cc65075c
williammanning68 <william.manning68@gmail.com>
2025-09-09 11:58:21 +1000

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -288,27 +288,30 @@ def extract_matches(text: str, keywords):
 # =============================================================================
 
 _EMPTY_MSOP_RE = re.compile(
-    r"<p\b[^>]*>"
-    r"(?:\s|&nbsp;|<br[^>]*>|"
-    r"<!--\[if.*?endif\]-->|"                 # MSO conditional comments
-    r"<o:p>.*?</o:p>|"
-    r"<span\b[^>]*>(?:\s|&nbsp;|<br[^>]*>|<!--\[if.*?endif\]-->|<o:p>.*?</o:p>)*</span>"
-    r")*"
-    r"</p>",
+    r"<p\b[^>]*>(?:\s|&nbsp;|<br[^>]*>|"
+    r"<!\[\s*if\s*!supportLineBreakNewLine\s*\]><br[^>]*><!\[\s*endif\s*\]>|"
+    r"<o:p>(?:\s|&nbsp;)*</o:p>|"
+    r"<span\b[^>]*>(?:\s|&nbsp;|<br[^>]*>)*</span>"
+    r")*</p>",
     re.I | re.S,
 )
 
 def _tighten_outlook_whitespace(html: str) -> str:
-    html = _EMPTY_MSOP_RE.sub("", html)
-    html = re.sub(r"(?:\s*<br[^>]*>\s*){2,}", "<br>", html, flags=re.I)
-    html = re.sub(r"(</table>)\s+(?=(?:<!--.*?-->\s*)*<table\b)", r"\1", html, flags=re.I | re.S)
-    html = re.sub(r">\s*(?:&nbsp;|<br[^>]*>|\s)+</td>", "></td>", html, flags=re.I)
+    # Kill Outlook's "downlevel-revealed" line-break block entirely
     html = re.sub(
-        r"(?:\s|&nbsp;|<br[^>]*>|<!--\[if.*?endif\]-->|<o:p>.*?</o:p>)+\s*(?=</body>)",
+        r"<!\[\s*if\s*!supportLineBreakNewLine\s*\]>\s*<br[^>]*>\s*<!\[\s*endif\s*\]>",
         "",
         html,
-        flags=re.I | re.S,
+        flags=re.I,
     )
+    # Remove empty Word/Outlook paragraphs (o:p, &nbsp;, spans, br, etc.)
+    html = _EMPTY_MSOP_RE.sub("", html)
+    # Collapse redundant <br>
+    html = re.sub(r"(?:\s*<br[^>]*>\s*){2,}", "<br>", html, flags=re.I)
+    # Close-up table-to-table gaps
+    html = re.sub(r"(</table>)\s+(?=(?:<!--.*?-->\s*)*<table\b)", r"\1", html, flags=re.I | re.S)
+    # Trim junk inside empty cells
+    html = re.sub(r">\s*(?:&nbsp;|<br[^>]*>|\s)+</td>", "></td>", html, flags=re.I)
     return html
 
 def _minify_inter_tag_whitespace(html: str) -> str:
